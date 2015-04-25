bc9d7f56ef117eb31e45b6e93f15d4e48d0341d8
Hannes Struss <x@hannesstruss.de>
2015-04-25 15:42:09 +0200

    added javascript build

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,45 @@
+/*jslint node: true */
+'use strict';
+
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     livereload = require('gulp-livereload'),
-    del = require('del');
+    del = require('del'),
+    watchify = require('watchify'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    gutil = require('gulp-util'),
+    sourcemaps = require('gulp-sourcemaps'),
+    assign = require('lodash').assign;
 
 gulp.task('styles', function() {
   return sass('src/styles/main.sass', {style: 'expanded'})
     .pipe(gulp.dest('dist/assets/css'));
 });
 
+var b = watchify(browserify(assign(watchify.args, {
+  entries: ['./src/js/main.js'],
+  debug: true
+})));
+
+function bundle_js() {
+  return b.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/assets/js'));
+}
+
+gulp.task('js', bundle_js);
+b.on('update', bundle_js);
+b.on('log', gutil.log);
+
 gulp.task('watch', function() {
   livereload.listen();
+  gulp.start('js');
   gulp.watch('src/styles/**/*.sass', ['styles']);
   gulp.watch(['dist/**']).on('change', livereload.changed);
 });
