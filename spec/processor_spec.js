2f24c9db5af959bdbce70dfdf56be41d5022d22b
Hannes Struss <x@hannesstruss.de>
2015-04-26 16:08:42 +0200

    parsing works

--- spec/processor_spec.js
+++ spec/processor_spec.js
@@ -24,6 +24,7 @@ describe('IngredientProcessor', function() {
     1.5 tbsp vinegar
     3.0 tsp orange juice`;
 
+
   let processor;
 
   beforeEach(() => {
@@ -56,8 +57,51 @@ describe('IngredientProcessor', function() {
     expect(result[1].name).toBe('vinegar');
 
     expect(result[2].amount).toBe(3);
-    expect(result[2].unit).toBe('tbsp');
+    expect(result[2].unit).toBe('tsp');
     expect(result[2].name).toBe('orange juice');
+  });
 
+  const literals = `
+    one egg
+    two cups of whisky
+    three quarts of a hamburger`;
+
+  it('parses literal numbers', function() {
+    let result = processor.parse(literals);
+
+    expect(result[0].amount).toBe(1);
+    expect(result[0].unit).toBe(null);
+    expect(result[0].name).toBe('egg');
+    
+    expect(result[1].amount).toBe(2);
+    expect(result[1].unit).toBe('cups');
+    expect(result[1].name).toBe('whisky');
+    
+    expect(result[2].amount).toBe(3);
+    expect(result[2].unit).toBe('quarts');
+    expect(result[2].name).toBe('hamburger');
+  });
+
+  it('parses indefinite amounts and units', function() {
+    let result = processor.parse('some coffee')[0];
+
+    expect(result.amount).toBe(null);
+    expect(result.unit).toBe(null);
+    expect(result.name).toBe('some coffee');
+  });
+
+  it('parses amounts', function() {
+    expect(processor.parseAmount('1 cup').amount).toBe(1);
+    expect(processor.parseAmount('1/2 cup').amount).toBe(0.5);
+    expect(processor.parseAmount('1 1/2 cups').amount).toBe(1.5);
+    expect(processor.parseAmount('1.5 cups').amount).toBe(1.5);
+    expect(processor.parseAmount('two cups').amount).toBe(2);
+  });
+
+  it('doesn\'t parse units/amounts in the end', function() {
+    let result = processor.parse('1 small shallot, minced (about 1 tablespoon)')[0];
+    expect(result.amount).toBe(1);
+    expect(result.unit).toBe(null);
+    expect(result.name).toBe('small shallot, minced (about 1 tablespoon)');
   });
 });
--- src/js/processor.js
+++ src/js/processor.js
@@ -1,16 +1,97 @@
 import Ingredient from './ingredient.js';
 
+const mixedFractionRe = /^(\d+)\s+(\d+)\/(\d+)\s+\w/;
+const fractionRe = /^(\d+)\/(\d+)\s+\w/;
+const numberRe = /^(\d+(\.(\d+))?)\s+\w/;
+
+const numbers = {
+	'one': 1,
+	'two': 2,
+	'three': 3,
+	'four': 4,
+	'five': 5,
+	'six': 6,
+	'seven': 7,
+	'eight': 8,
+	'nine': 9,
+	'ten': 10,
+	'eleven': 11,
+	'twelve': 12
+};
+const literalRe = new RegExp("^(" + Object.keys(numbers).join("|") + ")\\s+\\w");
+
+const units = [
+	'cup', 'tbsp', 'tsp', 'l', 'liters', 'pinch', 'pinches', 'quart'
+];
+const unitRe = new RegExp("^(" + units.map(u => u + "s?").join("|") + ")");
+
+const nameClutterRe = /^(of (an? )?)/;
+
 class IngredientProcessor {
   parse(ingredients) {
   	return ingredients
   		.split("\n")
   		.filter(x => /\S+/.test(x))
+  		.map(line => line.trim().toLowerCase())
   		.map(line => this.parseIngredient(line));
   }
 
   parseIngredient(line) {
-  	return new Ingredient(1.5, "tbsp", "Dieter");
+  	let amountResult = this.parseAmount(line);
+  	let amount = null;
+
+  	if (amountResult) {
+  		line = line.substr(amountResult.end - 1).trim();
+  		amount = amountResult.amount;
+  	}
+
+  	let unitM = unitRe.exec(line);
+  	let unit = null;
+  	let name = line;
+  	if (unitM) {
+  		unit = unitM[1];
+  		name = line.substr(unitM.index + unitM[0].length).trim();
+  	}
+  	return new Ingredient(amount, unit, this.sanitizeName(name));
   }
+
+  parseAmount(line) {
+  	let m = mixedFractionRe.exec(line);
+  	if (m) {
+  		return new Amount(
+  			parseInt(m[1], 10) + parseInt(m[2], 10) / parseInt(m[3], 10),
+  			m.index, m[0].length);
+  	}
+
+  	m = fractionRe.exec(line);
+  	if (m) {
+  		return new Amount(
+  			parseInt(m[1], 10) / parseInt(m[2], 10),
+  			m.index, m[0].length);
+  	}
+
+  	m = numberRe.exec(line);
+  	if (m) {
+  		return new Amount(parseFloat(m[1]), m.index, m[0].length);
+  	}
+
+  	m = literalRe.exec(line);
+  	if (m) {
+  		return new Amount(numbers[m[1]], m.index, m[0].length);
+  	}
+  }
+
+  sanitizeName(name) {
+  	return name.replace(nameClutterRe, "");
+  }
+}
+
+class Amount {
+	constructor(amount, start, length) {
+		this.amount = amount;
+		this.start = start;
+		this.end = start + length;
+	}
 }
 
 module.exports = IngredientProcessor;
