7650322d8078f212b3ceacbeb739e8572936219e
Hannes Struss <x@hannesstruss.de>
2015-05-10 18:28:49 +0200

    Formatting (oops) and more styling

--- src/js/view/view.jsx
+++ src/js/view/view.jsx
@@ -1,95 +1,94 @@
 import React from 'react';
-import Processor from '../processor.js'
+import Processor from '../processor.js';
 
 class RecipeInput extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: `
-				1 1/2 cups all-purpose flour
-				1 1/2 teaspoons baking powder
-				pinch of salt
-				2/3 cup sugar
-				6 tablespoons soft butter
-				2 eggs, beaten
-				3 bananas, mashed
-				2 tablespoons slivered almonds`.replace(/^\s+/gm, "").trim()
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+        // 1 1/2 cups all-purpose flour
+        // 1 1/2 teaspoons baking powder
+        // pinch of salt
+        // 2/3 cup sugar
+        // 6 tablespoons soft butter
+        // 2 eggs, beaten
+        // 3 bananas, mashed
+        // 2 tablespoons slivered almonds`.replace(/^\s+/gm, "").trim()
+    }
+  }
 
-	componentDidMount() {
-		this.props.onRecipeChanged(
-			React.findDOMNode(this.refs.textarea).value);
-	}
+  componentDidMount() {
+    this.props.onRecipeChanged(
+      React.findDOMNode(this.refs.textarea).value);
+  }
 
-	onChange(e) {
-		this.setState({
-			value: e.target.value
-		});
-		this.props.onRecipeChanged(e.target.value);
-	}
+  onChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+    this.props.onRecipeChanged(e.target.value);
+  }
 
-	render() {
-		return <textarea 
-			className="recipeInput"
-			ref="textarea"
-			onChange={this.onChange.bind(this)}
-			value={this.state.value}/>
-	}
+  render() {
+    return <textarea 
+      className="recipeInput"
+      ref="textarea"
+      placeholder="Paste an ingredient list..."
+      onChange={this.onChange.bind(this)}
+      value={this.state.value}/>
+  }
 }
 
 class IngredientList extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		var ingredientNodes = this.props.ingredients.map((ingredient) => {
-			return (
-				<ul className="ingredients-list">
-					<li key={ingredient.name}>
-						{ingredient.name} ({ingredient.unit}, {ingredient.amount})
-					</li>
-				</ul>
-			);
-		});
-
-		return (
-			<div>
-				<div className="ingredientList">
-				  {ingredientNodes}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <ul className="ingredients-list">
+        {this.props.ingredients.map((ingredient) => {
+          return <li key={ingredient.name} className="ingredient">
+            {ingredient.amount} {ingredient.unit} {ingredient.name}
+          </li>;
+        })}
+        <li>
+          <input type="text" className="recipeInput" placeholder="Add another ingredient..." />
+        </li>
+      </ul>
+    );
+  }
 }
 
 class RecipcalApp extends React.Component {
-	constructor(props) {
-		super(props);
-		this.processor = new Processor();
-		this.state = {
-			ingredients: []
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.processor = new Processor();
+    this.state = {
+      ingredients: []
+    };
+  }
 
-	onRecipeChanged(recipe) {
-		this.setState({
-			ingredients: this.processor.parse(recipe)
-		});
-	}
+  onRecipeChanged(recipe) {
+    var ingredients = this.processor.parse(recipe);
+    this.setState({
+      ingredients: ingredients
+    });
+  }
 
-	render() {
-		return <div className="app">
-			<RecipeInput onRecipeChanged={this.onRecipeChanged.bind(this)}/>
-			<IngredientList ingredients={this.state.ingredients} />
-		</div>;
-	}
+  render() {
+    var input = this.state.ingredients.length == 0;
+
+    return <div className="app">
+      {input ? <RecipeInput onRecipeChanged={this.onRecipeChanged.bind(this)}/> : null}
+      {!input ? <IngredientList ingredients={this.state.ingredients} /> : null}
+    </div>;
+  }
 }
 
 module.exports = function() {
-	React.render(
-	  <RecipcalApp />,
-	  document.querySelector('.content')
-	);
+  React.render(
+    <RecipcalApp />,
+    document.querySelector('.content')
+  );
 };
