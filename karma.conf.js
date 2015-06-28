af8acd40fce0b1708f825099eed2ff3634ae97c1
Hannes Struss <x@hannesstruss.de>
2015-06-28 11:54:25 +0200

    Add mocha reporter

--- karma.conf.js
+++ karma.conf.js
@@ -66,6 +66,8 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+
+    reporters: ['mocha']
   });
 };
