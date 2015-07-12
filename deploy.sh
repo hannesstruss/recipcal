6cecee367bab15d20372442661a5297ea0942e12
Hannes Struss <x@hannesstruss.de>
2015-07-12 21:39:43 +0200

    Add gh-pages deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+npm install
+npm install -g gulp-cli
+gulp build
+
+cd build
+git init
+
+git config user.name "Hannes Struss (Travis CI)"
+git config user.email "x@hannesstruss.de"
+
+git remote add origin "https://$GH_TOKEN@github.com/hannesstruss/recipcal.git"
+git fetch origin
+git reset origin/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "Rebuild gh-pages at ${rev}"
+git push -q origin HEAD:gh-pages
+
+rm -rf .git
